ee1890bd182c6463a5ac280c26131f1560e9bec1
hux <1198149984@qq.com>
2017-02-20 14:53:11 +0800

    first init

---
+++ util/util.js
@@ -0,0 +1,18 @@
+/**
+ * Created by benben on 17/2/20.
+ */
+module.exports = {
+    extend: function(target,source,flag){
+        for(var key in source){
+            if(source.hasOwnProperty(key)){
+                flag ?
+                    (target[key] = source[key]) :
+                    (target[key] === void 0 && (target[key] = source[key]));
+            }
+        }
+        return target;
+    },
+    isType: function(obj,type){
+        return Object.prototype.toString.call(obj) === '[object '+type+']';
+    }
+}
